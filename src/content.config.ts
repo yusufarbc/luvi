56d3c2c9f6c572124cf52759bdb01a1334011c8c
Yusuf Talha ARABACI <77548038+yusufarbc@users.noreply.github.com>
2026-03-22 14:38:41 +0300

    feat: Add `ServiceCards.astro` component to display services and configure the `services` content collection.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const services = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/services" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
